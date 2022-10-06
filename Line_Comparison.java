5f7a3b65cc530f2cc2d8e997b00e08a23c168615
SanketBezalwar <sanketbezalwar45@gmail.com>
2022-10-06 12:49:15 +0530

    Welcome Message

---
+++ Line_Comparison.java
@@ -0,0 +1,10 @@
+package Line_Comparison_Problem;
+
+public class Line_Comparison {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison");
+		System.out.println();
+	}
+
+}
